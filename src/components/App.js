72d50f70c793f2fe5976bbc2556bf350c5d78cbe
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-24 21:10:59 +0530

    Nikhil

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      <h1 data-ns-test="project-name">name</h1>
+      <h6 data-ns-test="project-description">description</h6>
+      <h1 data-ns-test="project-name">name1</h1>
+      <h6 data-ns-test="project-description">description1</h6>
+    </div>
   )
 }
 
